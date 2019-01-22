ddf32399a0ebb21b114389be3dbecf21ae1d63d8
blackjack201312 <blackjack201303@gmail.com>
2019-01-22 11:27:59 +0800

    add algo selection

--- caffe/set_path_algo.py
+++ caffe/set_path_algo.py
@@ -1,10 +1,17 @@
 import glob, os, re
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--algo', type=str, help='setting cudnn conv algorithm.', default='auto')
+parser.add_argument('--datapath', type=str, help='setting datapath for caffe prototxt.', default='/home/hpcl/data/caffe/fake_image_net.lmdb')
 
 network_filelist = glob.glob(r'networks/*.prototxt')
 network_filelist.sort()
 
-datapath = "/home/hpcl/data/caffe/fake_image_net.lmdb"
-algo = "auto"
+opt = parser.parse_args()
+datapath = opt.datapath
+algo = opt.algo
+#algo = "ipc_gemm" # auto, find, ipc_gemm, fft_tile, winograd_nonfused, winograd
 
 def set_network(filepath, datapath, algo):
 
