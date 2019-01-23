241665fc27fe2622e5000060509871b383582a94
WizardZhT <956234606@qq.com>
2019-01-23 14:02:19 +0800

    2

--- caffe/run.sh
+++ caffe/run.sh
@@ -1,11 +1,11 @@
 datapath=/home/hpcl/data/caffe/fake_image_net.lmdb
 
-python dvfs_benchmark.py --gpu-setting v100 --dl-setting dl_settings --algo auto --datapath $datapath
-python dvfs_benchmark.py --gpu-setting v100 --dl-setting dl_settings --algo ipc_gemm --datapath $datapath
+# python dvfs_benchmark.py --gpu-setting v100 --dl-setting dl_settings --algo auto --datapath $datapath
+# python dvfs_benchmark.py --gpu-setting v100 --dl-setting dl_settings --algo ipc_gemm --datapath $datapath
 python dvfs_benchmark.py --gpu-setting v100 --dl-setting dl_settings --algo fft_tile --datapath --datapath $datapath
-python dvfs_benchmark.py --gpu-setting v100 --dl-setting dl_settings --algo winograd_nonfused --datapath $datapath
+# python dvfs_benchmark.py --gpu-setting v100 --dl-setting dl_settings --algo winograd_nonfused --datapath $datapath
 
-python dvfs_benchmark.py --gpu-setting p100 --dl-setting dl_settings --algo auto --datapath $datapath
-python dvfs_benchmark.py --gpu-setting p100 --dl-setting dl_settings --algo ipc_gemm --datapath $datapath
-python dvfs_benchmark.py --gpu-setting p100 --dl-setting dl_settings --algo fft_tile --datapath $datapath
-python dvfs_benchmark.py --gpu-setting p100 --dl-setting dl_settings --algo winograd_nonfused --datapath $datapath
+# python dvfs_benchmark.py --gpu-setting p100 --dl-setting dl_settings --algo auto --datapath $datapath
+# python dvfs_benchmark.py --gpu-setting p100 --dl-setting dl_settings --algo ipc_gemm --datapath $datapath
+# python dvfs_benchmark.py --gpu-setting p100 --dl-setting dl_settings --algo fft_tile --datapath $datapath
+# python dvfs_benchmark.py --gpu-setting p100 --dl-setting dl_settings --algo winograd_nonfused --datapath $datapath
