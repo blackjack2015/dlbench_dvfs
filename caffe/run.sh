c60f4e75c6d3ad8d801a3fd9c9bac136885ac533
blackjack201312 <blackjack201303@gmail.com>
2019-01-28 14:00:55 +0800

    add v100 inference.

--- caffe/run.sh
+++ caffe/run.sh
@@ -10,4 +10,5 @@ datapath=/home/hpcl/data/caffe/fake_image_net.lmdb
 # python dvfs_benchmark.py --gpu-setting p100 --dl-setting dl_settings --algo ipc_gemm --datapath $datapath
 # python dvfs_benchmark.py --gpu-setting p100 --dl-setting dl_settings --algo fft_tile --datapath $datapath
 # python dvfs_benchmark.py --gpu-setting p100 --dl-setting dl_settings --algo winograd_nonfused --datapath $datapath
-python dvfs_benchmark.py --gpu-setting p100 --dl-setting inference
+#python dvfs_benchmark.py --gpu-setting p100 --dl-setting inference
+python dvfs_benchmark.py --gpu-setting v100 --dl-setting inference
